6f8aaf6b903e930f7f384324ec21f6c0ae3ee37a
kodemafia <dharmendramehta622@gmail.com>
2022-02-23 05:00:34 +0545

    customer hook done

---
+++ lib/controllers/timer_hook_controller.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+int useInfiniteTimer(BuildContext context) {
+  return use(const _InfiniteTimer());
+}
+
+class _InfiniteTimer extends Hook<int> {
+  const _InfiniteTimer();
+
+  @override
+  __InfiniteTimerState createState() => __InfiniteTimerState();
+}
+
+class __InfiniteTimerState extends HookState<int, _InfiniteTimer> {
+  late Timer _timer;
+  int _number = 0;
+
+  @override
+  void initHook() {
+    super.initHook();
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        _number = _timer.tick;
+      });
+    });
+  }
+
+  @override
+  int build(BuildContext context) {
+    return _number;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _timer.cancel();
+  }
+}
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,103 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:uichallenge/controllers/timer_hook_controller.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late Timer _timer;
+  int _number = 0;
+
+  @override
+  void initState() {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        _number = _timer.tick;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              _number.toString(),
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+}
+
+class HomePageHook extends HookWidget {
+  const HomePageHook({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _numberNotifier = useState(0);
+    useEffect(() {
+      final timer = Timer.periodic(
+        const Duration(seconds: 1),
+        (timer) {
+          _numberNotifier.value = timer.tick;
+        },
+      );
+      return timer.cancel;
+    }, const []);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomePage Hook'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              _numberNotifier.value.toString(),
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomePageCustomHook extends HookWidget {
+  const HomePageCustomHook({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final number = useInfiniteTimer(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomePage CustomHook'),
+      ),
+      body: Center(child: Text(number.toString())),
+    );
+  }
+}
